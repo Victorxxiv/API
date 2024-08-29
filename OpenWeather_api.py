b46c8b9842b4c2c69e65537806f3183c3495ce44
Victor <victormwendwa804@gmail.com>
2024-08-29 19:32:25 +0300

    Using OpenWeatherMap to check different Cities weather

--- OpenWeather_api.py
+++ OpenWeather_api.py
@@ -1,31 +1,36 @@
 import requests
 
 # API key from OpenWeather
-API_KEY = 'your_api_key'
-BASE_URL = 'http://api.openweathermap.org/data/2.5/weather?'
+API_KEY = '76b624388d75ae734318a1cb25064beb'
+BASE_URL = 'http://api.openweathermap.org/data/2.5/weather'
+
 
 # City name
 def get_weather(city_name):
     # API URL
     params = {
-        'q': city_name,
-        'appid': API_KEY,
+        'q': city_name,  # 'q' query parameter for city name
+        'appid': API_KEY,  # 'appid' query parameter for API key
         'units': 'metric'  # For temperature in Celsius
     }
-    response = requests.get(BASE_URL, params=params)
-
-    if response.status_code == 200:
-        data = response.json()
-        weather = {
-            'city': data['name'],
-            'temperature': data['main']['temp'],
-            'description': data['weather'][0]['description'],
-            'humidity': data['main']['humidity'],
-            'wind_speed': data['wind']['speed'],
-        }
-        return weather
-    else:
+    try:
+        response = requests.get(BASE_URL, params=params, timeout=10)  # Added timeout
+         
+        if response.status_code == 200:
+            data = response.json()
+            weather = {
+                'city': data['name'],
+                'temperature': data['main']['temp'],
+                'description': data['weather'][0]['description'],
+                'humidity': data['main']['humidity'],
+                'wind_speed': data['wind']['speed'],
+                }
+            return weather
+        
         return {'error': f"City {city_name} not found."}
+    
+    except requests.RequestException as e:
+        return {'error': f"An error occurred: {e}"}
 
 
 if __name__ == '__main__':
